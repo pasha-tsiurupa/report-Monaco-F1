b30044eb7a34990b38debb29bdceaf42dc95e810
Pavlo Tsiurupa <pavlotsiurupa@gmail.com>
2022-05-17 21:37:29 +0300

    update

--- report/report.py
+++ report/report.py
@@ -1,30 +1,31 @@
 import datetime
+import argparse
 
 
 def data_from_file(file_path):
     file_elements = []
     with open(file_path) as file:
         for line in file:
-            info = line.rstrip('\n')
+            info = line.rstrip()
             driver = info[:3]
-            date_time = datetime.datetime.strptime(info[3:].replace('_', ' '), '%Y-%m-%d %H:%M:%S.%f')
+            date_time = datetime.datetime.strptime('{0} {1}'.format(info[3:13], info[14:]), '%Y-%m-%d %H:%M:%S.%f')
             file_elements.append((driver, date_time))
     return file_elements
 
 
-def get_abbreviations():
+def get_abbreviations(folder_path):
     abbreviation_list = []
 
-    with open('D:/python/task-6-report-monaco-f1/data_files/abbreviations.txt', encoding='utf-8') as f:
+    with open(f'{folder_path}/abbreviations.txt', encoding='utf-8') as f:
         for line in f:
             abbreviation_list.append(line.rstrip('\n').split('_'))
     return abbreviation_list
 
 
-def build_report(file_path):
+def build_report(folder_path):
     res_list = []
-    start = data_from_file('D:/python/task-6-report-monaco-f1/data_files/start.log')
-    end = data_from_file('D:/python/task-6-report-monaco-f1/data_files/end.log')
+    start = data_from_file(f'{folder_path}/start.log')
+    end = data_from_file(f'{folder_path}/end.log')
     print()
     for item in start:
         for elem in end:
@@ -32,27 +33,41 @@ def build_report(file_path):
                 res_time = elem[1] - item[1]
                 res_list.append([item[0], abs(res_time)])
                 res_list.sort(key=lambda i: i[1])
-    with open(file_path, 'w') as file:
-        for line in res_list:
-            file.writelines('%s\n' % line)
     return res_list
 
 
-def print_report(file_path):
-    result = build_report(file_path)
-    abbreviations = get_abbreviations()
+def print_report(folder_path):
+    racers_rating = []
+    result = build_report(folder_path)
+    abbreviations = get_abbreviations(folder_path)
     place = 1
+    next_stage = 15
     for item in result:
         for elem in abbreviations:
             if item[0] == elem[0]:
-                print(str(place) + '.' + elem[1], '|', elem[2], '|', item[1])
+                racers_rating.append(f'{str(place)}.{elem[1]} |{elem[2]}| {item[1]}')
                 place += 1
-                if place == 16:
-                    print("-" * 80)
+                if place == next_stage + 1:
+                    racers_rating.append("-" * 65)
+    return racers_rating
 
 
 def main():
-    print_report('D:/python/task-6-report-monaco-f1/data_files/result.txt')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', '--files', required=True, type=str, help='Folder path')
+    parser.add_argument('--driver', type=str, help='Driver\'s name')
+    args = parser.parse_args()
+    rating = print_report(args.files)
+
+    if args.driver:
+        for driver in rating:
+            if args.driver in driver:
+                print(driver)
+
+            print(driver)
+    elif args.files:
+        for racer in rating:
+            print(racer)
 
 
 if __name__ == "__main__":
