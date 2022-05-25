0e070e2547fb00996d61788814f1831606b79ff9
Pavlo Tsiurupa <pavlotsiurupa@gmail.com>
2022-05-25 19:38:47 +0300

    upd

--- report/report.py
+++ report/report.py
@@ -1,16 +1,28 @@
-import datetime
 import argparse
-import sys
-import operator
+from dataclasses import dataclass
+from datetime import datetime
+from datetime import timedelta
 
 
+@dataclass
 class Driver:
-    def __init__(self, name, team):
-        self.name = name
-        self.team = team
+    abbreviation: str
+    name: str
+    team: str
+    time_start: str = None
+    time_end: str = None
 
-    def __str__(self):
-        return f'{self.name}, {self.team}'
+    @property
+    def lap_time(self):
+        try:
+            time_delta = datetime.fromisoformat(self.time_end) - datetime.fromisoformat(self.time_start)
+            if time_delta.days < 0 or time_delta.seconds > 1200:
+                self.best_time = 'Error'
+            else:
+                self.best_time = time_delta
+        except:
+            self.best_time = 'Error'
+        return self.best_time
 
 
 def data_from_file(file_path):
@@ -19,8 +31,8 @@ def data_from_file(file_path):
         for line in file:
             info = line.rstrip()
             driver = info[:3]
-            date_time = datetime.datetime.strptime(info[3:], '%Y-%m-%d_%H:%M:%S.%f')
-            file_elements[driver] = date_time
+            time = info[3:].replace('_', ' ').rstrip('\n')
+            file_elements[driver] = time
     return file_elements
 
 
@@ -30,75 +42,83 @@ def get_abbreviations(folder_path):
     with open(f'{folder_path}/abbreviations.txt', encoding='utf-8') as f:
         for line in f:
             abbr, name, team = line.rstrip('\n').split('_')
-            abbreviations[abbr] = str(Driver(name=name, team=team))
-        return abbreviations
+            abbreviations[abbr] = [name, team]
+    return abbreviations
 
 
-def build_report(folder_path, *order):
-    start = data_from_file(f'{folder_path}/start.log')
-    end = data_from_file(f'{folder_path}/end.log')
-    res_dict = {}
-    for abbr, time in end.items():
-        result_time = abs(time - start[abbr])
-        res_dict.update({abbr: result_time})
-    result_list = []
-    abbreviations = get_abbreviations(folder_path)
-    if order == ():
-        result = sorted(res_dict.items(), key=operator.itemgetter(1))
-        for abbr, time in result:
-            name, team = abbreviations[abbr].split(',')
-            result_list.append('{:<17} |{:<27}| {}'.format(
-                name, team, time))
-    else:
-        result = sorted(res_dict.items(), key=operator.itemgetter(1), reverse=True)
-        for abbr, time in result:
-            name, team = abbreviations[abbr].split(',')
-            result_list.append('{:<17}|{:<27}| {}'.format(
-                name, team, time))
-    return result_list
+def join_info(racer_info, time_start, time_end):
+    result = []
+    for key in racer_info:
+        start = time_start.get(key)
+        end = time_end.get(key)
+        result.append(Driver(key, racer_info[key][0], racer_info[key][1], start, end))
+    return result
 
 
-def print_report(folder_path, *desc):
-    report = build_report(folder_path)
-    next_stage = 15
-    if desc == ():
-        for place, driver in enumerate(report, 1):
-            print(place, driver)
-            if place == next_stage:
-                print("-" * 65)
-    else:
-        for place, driver in reversed(list(enumerate(report, 1))):
-            print(place, driver)
-            if place == next_stage + 1:
-                print("-" * 65)
+def load_data(path):
+    racer_info = get_abbreviations(path)
+    time_start = data_from_file(f'{path}/start.log')
+    time_end = data_from_file(f'{path}/end.log')
+    return join_info(racer_info, time_start, time_end)
 
 
-def parse_args(args):
+def get_attribute_max_length(data, attr_name):
+    return max(map(lambda x: len(getattr(x, attr_name)), data))
+
+
+def build_report(folder_path):
+    data = load_data(folder_path)
+    max_len_name = get_attribute_max_length(data, 'name')
+    max_len_team = get_attribute_max_length(data, 'team')
+    ready_report = []
+    place = 1
+    report = sorted(data, key=lambda time: time.lap_time if isinstance(time.lap_time, timedelta) else timedelta.max)
+    for racer in report:
+        if place == 16:
+            ready_report.append('-' * 65)
+        ready_report.append(
+            (f'{{0:2}}. {{1:{max_len_name}}} | {{2:{max_len_team}}} | {{3:11.11}}').format(place,
+                                                                                           racer.name, racer.team,
+                                                                                           str(racer.lap_time)))
+        place += 1
+    return ready_report
+
+
+def print_report(path, asc_sort=True):
+    result = build_report(path)
+    if not asc_sort:
+        result.reverse()
+    for string in result:
+        print(string)
+    return result
+
+
+def get_racer_info(path, racer_name):
+    data = load_data(path)
+    for racer in data:
+        if racer.name == racer_name:
+            return f'{racer.name} | {racer.team} | {racer.lap_time}'
+
+
+def parse_args(*args):
     parser = argparse.ArgumentParser()
-    parser.add_argument('-f', '--files', required=True, type=str, help='Folder path')
+    parser.add_argument('--files', type=str, help='Folder path')
     parser.add_argument('--driver', type=str, help='Driver\'s name')
     group = parser.add_mutually_exclusive_group()
-    group.add_argument('--asc', action='store_true')
-    group.add_argument('--desc', action='store_true')
-    return parser.parse_args(args)
+    group.add_argument('--asc', action='store_const', const=True, help='Ascending sort')
+    group.add_argument('--desc', action='store_const', const=True, help='Descending sort')
+    return parser.parse_args(*args)
 
 
-def main(command_line_args):
-    args = parse_args(command_line_args)
-
+def main():
+    args = parse_args()
     if args.files is not None:
         if args.driver:
-            results = build_report(args.files)
-            for driver in results:
-                if args.driver in driver:
-                    print(driver)
-
-        elif args.desc:
-            print_report(args.files, args.desc)
-
-        elif args.files:
-            print_report(args.files)
+            print(get_racer_info(args.files, args.driver))
+        else:
+            asc = False if args.desc else True
+            print_report(args.files, asc)
 
 
-if __name__ == "__main__":
-    main(sys.argv[1:])
+if __name__ == '__main__':
+    main()
